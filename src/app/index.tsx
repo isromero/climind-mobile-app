03a283774cb5467fb682d06a50775ae4e6c99b11
isromero <ismaromeroortega@gmail.com>
2024-12-08 20:20:24 +0100

    Configure ApolloProvider in local and production

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,30 @@
 import { AppNavigation } from "../navigation/AppNavigation";
 import "../utils/firebase"; // Initialize Firebase
 import Toast from "react-native-toast-message";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+// Check if the app is running in development or production
+let uri = "";
+const node_env = process.env.NODE_ENV;
+if (node_env === "development") {
+  uri = "http://localhost:4000/graphql";
+  console.log("Running in development mode");
+} else {
+  uri = "https://app.climind.com/api/";
+  console.log("Running in production mode");
+}
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri,
+  cache: new InMemoryCache(),
+});
 
 export default function Index() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <AppNavigation />
       <Toast />
-    </>
+    </ApolloProvider>
   );
 }
